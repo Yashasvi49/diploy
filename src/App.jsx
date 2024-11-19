5d135ab98ad8463cf17aa58149f5491061331563
Yashasvi49 <ssuhani387@gmail.com>
2024-11-19 18:38:40 +0530

    .edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
     <div className="bg-red-400 ">
     <div>App</div>
     <div>App</div>
-    <H1>{import.meta.env.VITE_CHATGPT_KEY}</H1>
+    {/* <H1>{import.meta.env.VITE_CHATGPT_KEY}</H1> */}
 
     </div>
   )
