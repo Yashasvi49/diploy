0f297e81fded95ac57bc51c944790bf19ad97c98
Yashasvi49 <ssuhani387@gmail.com>
2024-11-19 18:28:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div className="bg-red-400 ">
+    <div>App</div>
+    <div>App</div>
+    <H1>{import.meta.env.VITE_CHATGPT_KEY}</H1>
+
+    </div>
+  )
+}
+
+export default App 
